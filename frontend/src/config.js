946bd52f941735df0b2a672c65dcc0a419e42c70
sodipe <98396909+so-dipe@users.noreply.github.com>
2024-01-09 03:05:03 +0000

    Update BaseUrl in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const BaseUrl = process.env.REACT_APP_BASE_URL || "https://socialmedia-frontend-4s2v.onrender.com";
+export const BaseUrl = process.env.REACT_APP_BASE_URL || "https://socialmediaapp-backend-ptjrsgbh3q-uc.a.run.app";
 console.log("BASE_URL", BaseUrl)
 export const wsUrl = `ws://${BaseUrl.split("//")[1]}`;
 console.log("WS_URL", wsUrl)
