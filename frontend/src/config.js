369a99bd9d1fc7bd213b6d955076b5649f2ffa60
sodipe <98396909+so-dipe@users.noreply.github.com>
2024-01-09 03:07:07 +0000

    Update WebSocket URL in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
+export const wsProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
 export const BaseUrl = process.env.REACT_APP_BASE_URL || "https://socialmediaapp-backend-ptjrsgbh3q-uc.a.run.app";
 console.log("BASE_URL", BaseUrl)
-export const wsUrl = `ws://${BaseUrl.split("//")[1]}`;
+export const wsUrl = `${wsProtocol}//${BaseUrl.split("//")[1]}`;
 console.log("WS_URL", wsUrl)
